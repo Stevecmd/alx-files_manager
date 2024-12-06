66715a0e00f6d5653a158c56cb1aaa0f3eac4081
SteveCmd <murimi101@gmail.com>
2024-12-06 15:28:22 +0300

    4. Authenticate a user: Implement authentication endpoints and user retrieval

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 class UsersController {
   static async postNew (req, res) {
@@ -22,6 +24,27 @@ class UsersController {
     const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
 
     return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe (req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
